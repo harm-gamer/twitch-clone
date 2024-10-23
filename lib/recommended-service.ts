09eefc56c959c324403e3023acd182a8805719f0
harm-gamer <mukundt513@gmail.com>
2024-10-23 13:09:41 +0530

    recommended-service

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -5,14 +5,15 @@ import { db } from "./db"
 export const getRecommended = async () =>{
 
     await new Promise(resolve => setTimeout(resolve,5000))
+    let userId;
     try{
         const self = await  getSelf();
-        userId = self.Id
+        userId = self.id
     }
     catch{
         userId = null;
     }
-   
+  
     const users = await db.user.findMany({
      orderBy : {
         createdAt : "desc"
